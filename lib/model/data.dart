9ed9a474efc7d103e4f62a0c0157689a62b74eae
irvanff <firmansyahirvan70@gmail.com>
2021-10-05 23:04:23 +0700

    push

--- lib/model/data.dart
+++ lib/model/data.dart
@@ -7,5 +7,20 @@ class Data {
         email: 'Firmansyahirvan@mail.com',
         alamat: 'jl.bima raya no 3',
         noTelp: 08945552989),
+    Users(
+        nama: 'Agus',
+        email: 'AgusSupratno@mail.com',
+        alamat: 'jl.arjuna raya no 4',
+        noTelp: 089764353924),
+    Users(
+        nama: 'stuwart',
+        email: 'littlestuwart@mail.com',
+        alamat: 'jl.agusta raya no 5',
+        noTelp: 0876534949243),
+    Users(
+        nama: 'Fajar',
+        email: 'Fajarbintang@mail.com',
+        alamat: 'jl.gunung raya no 6',
+        noTelp: 08633375394745),
   ];
 }
